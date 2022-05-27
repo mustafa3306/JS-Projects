966c3d42be4b5a7fde118b6229fab57e98204bd0
mustafa3306 <mustafaworkkok@gmail.com>
2022-05-27 12:40:54 +0300

    last example added

--- ObjectExamples/object.js
+++ ObjectExamples/object.js
@@ -35,13 +35,24 @@ const kisiler = [
 
 //* Example-1 ---> List the people who younger than 33.
 
+console.log("*********Example-1*********");
 
 
 const littles = kisiler.filter((yas) => yas.age < 33).forEach((ad) => console.log(ad.name));
 
 //* Example-2 ---> Please keep the names of those under 33 years old in the array
-
+console.log("*******Example-2***********");
 const littleName = kisiler.filter((yas) => yas.age < 33).map((nm) => nm.name);
 
 console.log(littleName);
 
+//* Example-3 ---> (Yaşı 33 ten küçük olanların isim ve yaşlarının obje halinde bir listede göster.)
+console.log("*********Example-3***********");
+
+const isYas = kisiler
+  .filter((meslek) => meslek.job == "developer")
+  .map((is) => ({
+    name: is.name,
+    age: is.age,
+  }));
+console.log(isYas);
