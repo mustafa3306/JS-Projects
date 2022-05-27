f1663de21fb57773b3fa9e4617e123a834d782ab
mustafa3306 <mustafaworkkok@gmail.com>
2022-05-27 12:19:44 +0300

    main,

--- ObjectExamples/object.js
+++ ObjectExamples/object.js
@@ -1,1 +1,41 @@
 ////////*In this file, we will do some examples about javascript objects.
+
+//* Example-1 ---> List the people who younger than 33.
+
+const kisiler = [
+    {
+      name: "Ahmet",
+      surname: "Can",
+      job: "developer",
+      age: 30,
+    },
+    {
+      name: "Mehmet",
+      surname: "Baki",
+      job: "tester",
+      age: 35,
+    },
+    {
+      name: "Nur",
+      surname: "Ersan",
+      job: "team lead",
+      age: 40,
+    },
+    {
+      name: "Merve",
+      surname: "Veli",
+      job: "developer",
+      age: 26,
+    },
+  
+    {
+      name: "Ruzgar",
+      surname: "Kuzey",
+      job: "tester",
+      age: 24,
+    },
+  ];
+
+
+const littles = kisiler.filter( (yas) => yas.age < 33 ).forEach();
+
